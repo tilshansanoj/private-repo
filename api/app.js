64179587ada7225ac02e24016a8b44d25b92d77f
tilshansanoj <tilshansanoj@gmail.com>
2024-11-26 11:45:21 +0000

    final commit

--- api/app.js
+++ api/app.js
@@ -2,61 +2,67 @@ var express = require('express');
 var app = express();
 var uuid = require('node-uuid');
 
-var pg = require('pg');
-var conString = process.env.DB; // "postgres://username:password@localhost/database";
+const { Pool } = require('pg');
 
-// Routes
-app.get('/api/status', function(req, res) {
-  pg.connect(conString, function(err, client, done) {
-    if(err) {
-      return res.status(500).send('error fetching client from pool');
+const pool = new Pool({
+  connectionString: process.env.DB,
+});
+
+
+app.get('/api/status', function (req, res) {
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.error('error fetching client from pool', err);
+      return;
     }
-    client.query('SELECT now() as time', [], function(err, result) {
+    client.query('SELECT now() as time', [], function (err, result) {
       //call `done()` to release the client back to the pool
       done();
 
-      if(err) {
-        return res.status(500).send('error running query');
-      }
 
+      if (err) {
+        console.error('error running query', err);
+        return;
+      }
       return res.json({
         request_uuid: uuid.v4(),
         time: result.rows[0].time
+
       });
     });
   });
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handlers
+  // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
-});
 
 
-module.exports = app;
+  module.exports = app;
